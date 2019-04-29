1b9269de1f02ac5c7f703a3e123bd59ea23680d8
Jack Lindamood <jlindamo@justin.tv>
2019-04-29 13:32:48 -0700

    Restructure tests

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/cep21/gobenchtraces
 
 go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.19.19 // indirect
+	github.com/aws/aws-xray-sdk-go v1.0.0-rc.11
+	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/newrelic/go-agent v2.7.0+incompatible
+	github.com/opentracing/opentracing-go v1.1.0
+	github.com/philhofer/fwd v1.0.0 // indirect
+	github.com/stretchr/testify v1.3.0
+	github.com/tinylib/msgp v1.1.0 // indirect
+	github.com/uber-go/atomic v1.3.2 // indirect
+	github.com/uber/jaeger-client-go v2.16.0+incompatible
+	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
+	go.uber.org/atomic v1.3.2 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
+	golang.org/x/sys v0.0.0-20190429094411-2cc0cad0ac78 // indirect
+	gopkg.in/DataDog/dd-trace-go.v1 v1.12.1
+)
---
+++ xray_test.go
@@ -0,0 +1,304 @@
+package gobenchtraces
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-xray-sdk-go/xray"
+	"github.com/aws/aws-xray-sdk-go/xraylog"
+	newrelic "github.com/newrelic/go-agent"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"github.com/stretchr/testify/require"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"golang.org/x/sync/errgroup"
+	"sync"
+	"testing"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"time"
+)
+
+type service interface {
+	Setup() error
+	Start() error
+	Stop() error
+}
+
+type multiService struct {
+	services []service
+}
+
+func (m *multiService) Setup() error {
+	for _, e := range m.services {
+		if err := e.Setup(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *multiService) Start() error {
+	ctx := context.Background()
+	eg, _ := errgroup.WithContext(ctx)
+	for _, e := range m.services {
+		eg.Go(e.Start)
+	}
+	return eg.Wait()
+}
+
+func (m *multiService) Stop() error {
+	for _, e := range m.services {
+		if err := e.Stop(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type xrayTrial struct {
+}
+
+func (m *xrayTrial) Setup() error {
+	xray.SetLogger(xraylog.NullLogger)
+	return nil
+}
+
+func (m *xrayTrial) Start() error {
+	return nil
+}
+
+func (m *xrayTrial) Stop() error {
+	return nil
+}
+
+type ddTrial struct {
+	server http.Server
+}
+
+func (m *ddTrial) Setup() error {
+	log.SetOutput(ioutil.Discard)	// Yes, the DD logger does this :(
+	tracer.Start(tracer.WithServiceName("test"))
+	tracer.WithPrioritySampling()
+	return nil
+}
+
+func (m *ddTrial) Start() error {
+	m.server.Addr = ":8126"
+	m.server.Handler = m
+	return m.server.ListenAndServe()
+}
+
+func (m *ddTrial) ServeHTTP(rw http.ResponseWriter, _ *http.Request) {
+	rw.WriteHeader(http.StatusOK)
+}
+
+func (m *ddTrial) Stop() error {
+	return m.server.Close()
+}
+
+type jaegerTrial struct {
+	onClose io.Closer
+}
+
+func (m *jaegerTrial) Setup() error {
+	cfg, err := jaegercfg.FromEnv()
+	if err != nil {
+		return err
+	}
+	cfg.ServiceName = "test"
+	openTracer, closer, err := cfg.NewTracer(jaegercfg.Sampler(jaeger.NewConstSampler(true)))
+	if err != nil {
+		return err
+	}
+	m.onClose = closer
+	opentracing.SetGlobalTracer(openTracer)
+	return nil
+}
+
+func (m *jaegerTrial) Start() error {
+	return nil
+}
+
+func (m *jaegerTrial) Stop() error {
+	if m.onClose != nil {
+		return m.onClose.Close()
+	}
+	return nil
+}
+
+type nrTrial struct {
+	app newrelic.Application
+}
+
+func (m *nrTrial) Setup() error {
+	config := newrelic.NewConfig("Your Application Name", "0123456789012345678901234567890123456789")
+	config.Logger = newrelic.NewLogger(ioutil.Discard)
+	app, err := newrelic.NewApplication(config)
+	if err != nil {
+		return err
+	}
+	m.app = app
+
+	return nil
+}
+
+func (m *nrTrial) Start() error {
+	return nil
+}
+
+func (m *nrTrial) Stop() error {
+	m.app.Shutdown(time.Second)
+	return nil
+}
+
+var _ http.HandlerFunc = emptyHandler
+
+func emptyHandler(rw http.ResponseWriter, req *http.Request) {
+}
+
+func emptyTestFunc() {
+
+}
+
+type runBag struct {
+	nrApp newrelic.Application
+}
+
+type goroutineBag struct {
+	rw *httptest.ResponseRecorder
+	req *http.Request
+}
+
+func (g *goroutineBag) setup() {
+	var err error
+	g.req, err = http.NewRequest("POST", "www.example.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	g.rw = httptest.NewRecorder()
+}
+
+type benchmarkTracesRun struct {
+	name string
+
+	atOnce int
+	trace func(b *testing.B, run benchmarkTracesRun)
+	toCall func()
+
+	runBag runBag
+	goroutineBag goroutineBag
+}
+
+func openjaegerRun(b *testing.B, run benchmarkTracesRun) {
+	ctx := context.Background()
+	span, _ := opentracing.StartSpanFromContext(ctx, "operation_name")
+	run.toCall()
+	span.Finish()
+}
+
+func xrayRun(b *testing.B, run benchmarkTracesRun) {
+	ctx := context.Background()
+	_, s := xray.BeginSegment(ctx, "start")
+	run.toCall()
+	s.Close(nil)
+}
+
+func ddRun(b *testing.B, run benchmarkTracesRun) {
+	span := tracer.StartSpan("test")
+	run.toCall()
+	span.Finish(tracer.WithError(nil))
+}
+
+func newRelicRun(b *testing.B, run benchmarkTracesRun) {
+	txn := run.runBag.nrApp.StartTransaction("myTxn", run.goroutineBag.rw, run.goroutineBag.req)
+	require.NoError(b, txn.End())
+}
+
+func BenchmarkTraces(b *testing.B) {
+	b.ReportAllocs()
+	nrT := nrTrial{}
+	ms := multiService {
+		services: []service{
+			&ddTrial{}, &jaegerTrial{}, &nrT, &xrayTrial{},
+		},
+	}
+	require.NoError(b, ms.Setup())
+	go func() {
+		require.NoError(b, ms.Start())
+	}()
+	const upper = 1000
+	runs := []benchmarkTracesRun{
+		{
+			name: "x-ray",
+			atOnce: 1,
+			trace: xrayRun,
+			toCall: emptyTestFunc,
+		},
+		{
+			name: "x-ray",
+			atOnce: upper,
+			trace: xrayRun,
+			toCall: emptyTestFunc,
+		},
+		{
+			name: "datadog",
+			atOnce: 1,
+			trace: ddRun,
+			toCall: emptyTestFunc,
+		},
+		{
+			name: "datadog",
+			atOnce: upper,
+			trace: ddRun,
+			toCall: emptyTestFunc,
+		},
+		{
+			name: "openjaeger",
+			atOnce: 1,
+			trace: openjaegerRun,
+			toCall: emptyTestFunc,
+		},
+		{
+			name: "openjaeger",
+			atOnce: upper,
+			trace: openjaegerRun,
+			toCall: emptyTestFunc,
+		},
+		{
+			name: "newrelic",
+			atOnce: 1,
+			trace: newRelicRun,
+			toCall: emptyTestFunc,
+		},
+		{
+			name: "newrelic",
+			atOnce: upper,
+			trace: newRelicRun,
+			toCall: emptyTestFunc,
+		},
+	}
+	for _, run := range runs {
+		b.Run(fmt.Sprintf("%s-%d", run.name, run.atOnce), func(b *testing.B) {
+			run := run
+			run.runBag.nrApp = nrT.app
+			wg := sync.WaitGroup{}
+			for ao := 0;ao<run.atOnce;ao++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					run2 := run
+					run2.goroutineBag.setup()
+					for i:=0;i<b.N / run2.atOnce;i++ {
+						run2.trace(b, run2)
+					}
+				}()
+			}
+			wg.Wait()
+		})
+	}
+	require.NoError(b, ms.Stop())
+}
